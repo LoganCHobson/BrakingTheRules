a24340ec515f0b4d95bbbc58ea19842aa42e63c5
LogansPc <loganhobson31@gmail.com>
2022-09-18 02:51:12 -0500

    AI is good to go.

--- Assets/Scripts/CustomAIMovementArcher.cs
+++ Assets/Scripts/CustomAIMovementArcher.cs
@@ -5,67 +5,58 @@ using Pathfinding;
 
 public class CustomAIMovementArcher : MonoBehaviour
 {
+   
     private Transform target;
-    private Transform playerTransform;
-    private Transform bowTransform;
-    private bool shoot;
 
     public float radius;
-    
-
-    public Transform firingpoint;
-    public Transform enemyGFX;
+    public Transform raycastPos;
     public GameObject player;
-    public GameObject bow;
-    public GameObject arrow;
-    
+
     public float speed = 200f;
     public float nextWaypointDistance = 3f;
     public float pathUpdateSpeed = .2f;
-    public float AOA = 100; //Area of Awareness
-    public bool AOAToggle = false;
     public bool randomizerToggle = false;
 
     private Vector2 force;
-    
-     IAstarAI ai;
+
     Path path;
     int currentWaypoint = 0;
     bool reachedEndOfPath = false;
     Seeker seeker;
     Rigidbody2D rb;
 
-
+    public Transform[] moveSpots;
+    private int randomSpot;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(randomizerToggle == true)
+        if (randomizerToggle == true)
         {
             Randomizer();
         }
-        
+
         player = GameObject.Find("Player");
         target = player.GetComponent<Transform>();
-        bowTransform = bow.GetComponent<Transform>();
 
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
 
         InvokeRepeating("UpdatePath", 0f, pathUpdateSpeed);
+
+        seeker.StartPath(rb.position, moveSpots[randomSpot].position, OnPathComplete);
+
     }//End of Start
 
     void FixedUpdate() //Ideal when working with physics
     {
-        
-        Ranged();
 
-        if(path == null)
+        if (path == null)
         {
             return;
         }
 
-        if(currentWaypoint >= path.vectorPath.Count)
+        if (currentWaypoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = true;
         }
@@ -78,18 +69,20 @@ public class CustomAIMovementArcher : MonoBehaviour
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         force = direction * speed * Time.deltaTime;
         rb.AddForce(force);
-        
-        Flip();
-        
+
+        //Flip();
+
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
-        if(distance < nextWaypointDistance)
+        if (distance < nextWaypointDistance)
         {
             currentWaypoint++;
         }
-    }//End of FixedUpdate
-    
-     void OnPathComplete(Path p)
+        
+        
+        }//End of FixedUpdate
+
+    void OnPathComplete(Path p)
     {
         if (!p.error)
         {
@@ -98,31 +91,19 @@ public class CustomAIMovementArcher : MonoBehaviour
         }
     }//End of OnPathComplete
 
-    void UpdatePath()
+    void UpdatePath() //This functions runs at update.
     {
         float distance = Vector2.Distance(target.position, transform.position);
 
-        if(seeker.IsDone())
+        if (seeker.IsDone())
         {
-            if(distance <= AOA && AOAToggle == true)
-            {
-                seeker.StartPath(rb.position, target.position, OnPathComplete);
-            }
-            if(AOAToggle == false)
-            {
-                seeker.StartPath(rb.position, target.position, OnPathComplete);
-            }
+            //if (distance <= AOA && AOAToggle == true) //So long as the distance of the player is less than the AOA and the aoa toggle is active
+            
+                Ranged(); //execute ranged
+            
+            
         }
     }// End of UpdatePath
-
-    void OnDrawGizmosSelected() //Drawing AOA
-    {
-        if(AOAToggle == true)
-        {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, AOA);
-        }
-    }
 
     void Randomizer()
     {
@@ -134,44 +115,46 @@ public class CustomAIMovementArcher : MonoBehaviour
 
     void Ranged()
     {
-        Vector2 targetPos = target.position;
-        Vector2 Direction;
-        Direction = targetPos - (Vector2)transform.position;
-        RaycastHit2D ray = Physics2D.Raycast(bow.transform.position, Direction);
-        if(ray.transform.gameObject != null && ray.transform.gameObject.tag != "Player")
+        Vector2 targetPos = target.position; //Setting target pos to player position as a vector2
+        Vector2 Direction; //Declaring direction vec2
+        Direction = targetPos - (Vector2)transform.position; //Getting the difference of the two positions
+        RaycastHit2D[] ray = Physics2D.RaycastAll(raycastPos.transform.position, Direction); //Shooting a ray cast array of atleast two to see what we hit at the bow position heading in the direction of the difference
+        if (ray[0].transform.gameObject != null && ray[0].transform.gameObject.tag == "Player") //If ray is not null, which means if it is there, and so long as tag is no player
         {
-            Debug.Log(ray.transform.name);
-        
-            ai.destination = PickRandomPoint();
+            Debug.Log(ray[0].transform.gameObject.name); //Tell us what the raycast is
+            seeker.StartPath(rb.position, target.position, OnPathComplete); //move
         }
+            if (ray[0].transform.gameObject != null && ray[0].transform.gameObject.tag != "Player" && currentWaypoint >= path.vectorPath.Count) //If ray is not null, which means if it is there, and so long as tag is no player
+            {
+                seeker.StartPath(rb.position, moveSpots[randomSpot].position, OnPathComplete);
+
+                if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+                {
+                    randomSpot = Random.Range(0, moveSpots.Length);
+
+                }
+            }
+
         else
         {
             return;
         }
     }
 
-        void Flip()
+    /*void Flip()
+    {
+        if (force.x >= 0.01f)
         {
-            if(force.x <= 0.01f)
-            {
-                
-                enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
-                firingpoint.localScale = new Vector3(-1f, 1f, 1f);
-            }
-            else if(force.x >= -0.01f)
-            {
-                
-                enemyGFX.localScale = new Vector3(1f, 1f, 1f);
-                firingpoint.localScale = new Vector3(1f, 1f, 1f);
-            }
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+            firingpoint.localScale = new Vector3(-1f, 1f, 1f);
         }
-        
-        Vector3 PickRandomPoint () 
+        else if (force.x <= -0.01f)
         {
-            var point = Random.insideUnitSphere * radius;
 
-            point.y = 0;
-            point += ai.position;
-            return point;
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+            firingpoint.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
+    */
 }
+    
